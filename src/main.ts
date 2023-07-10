cd74d85cce902464fc065ea0ec1e67a7da7af07b
Sergio Diez <diezgarciasergioo@gmail.com>
2023-07-10 18:00:08 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,35 @@
-import "./style.css";
+const inicializaBotones = () => {
+  const botonAtras = document.getElementById("atras");
+  if (botonAtras instanceof HTMLButtonElement) {
+    botonAtras.addEventListener("click", () => {
+      console.log("Atras pulsado");
+    });
+  }
+};
 
-console.log("Hello Typescript!");
+inicializaBotones();
+
+/*
+function numero() {
+  const siguiente = document.getElementById("siguiente");
+  const atras = document.getElementById("atras");
+
+  if (siguiente) {
+    siguiente.addEventListener("click", () => {
+      const numeroTurno = document.getElementById("numero-turno");
+      console.log(numeroTurno);
+      
+      if(numeroTurno instanceof ) {
+        let valorTurno = parseInt(numeroTurno.textContent);
+        valorTurno += 1;
+        numeroTurno.textContent = valorTurno.toString();  
+      }
+      
+    });
+  }
+
+  if (atras) {
+    atras.addEventListener("click", () => {});
+  }
+}
+*/
